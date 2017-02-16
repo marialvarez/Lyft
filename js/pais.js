fdc8a5e1238ce2a92ad826e21193039eb2ef609b
Maricela Alvarez <malvarezdrobush@gmail.com>
2017-02-16 12:50:27 -0500

    listo

--- js/pais.js
+++ js/pais.js
@@ -1,9 +1,39 @@
+var inputNumber = document.getElementById("number");
+
 function init(){
+
     var button = document.getElementById("next");
     button.addEventListener('click',onButtonClick);
+
 }
 
 function onButtonClick() {
-    var input = document.getElementById("number");
-    localStorage.setItem('Number', input.value);
+    
+    localStorage.setItem('Number', inputNumber.value);
+    
+    var mensajeNumber = document.getElementById("mensajeNumber");
+    
+    if(inputNumber.value == '') {
+        mensajeNumber.innerHTML = "<br><h5 class='text-center' style='color:#FF1493';>Debes escribir tu número telefónico</h5>"
+    } else {
+        mensajeNumber.innerHTML = '';
+        location.href = 'signup.html';
+    }
+    
+    
 }
+
+
+function validateNumber(_evt){
+    
+    number = window.event.keyCode;
+
+
+    if((number>=48 && number<=57)|| number==8 || number==32){
+            
+    } else {
+        
+        _evt.preventDefault();
+    }     
+} 
+	
